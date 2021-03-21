14f3a0ff1eb3dbc32b4cfb8252f20c0679a7b2a9
Matthieu Bruneaux <matthieu.bruneaux@gmail.com>
2021-03-21 17:37:04 +0200

    Fix documentation for example file sequences-test-matching.xlsx

--- R/extdata-doc.R
+++ R/extdata-doc.R
@@ -1,22 +1,24 @@
 # Documenting data files in inst/extdata/ following advice from this thread:
 # https://stackoverflow.com/questions/35373257/r-and-roxygen2-how-to-document-data-files-in-inst-extdata
 
-#' Excel file 'sequences-test-matching.xlsx'
+#' @title Example files shipped with the package
+#' @name example-files-shipped-with-the-package
+#' 
+#' @description
+#' Several example files are shipped with the concatipede package.
 #'
-#' This is an Excel file (.xlsx) typically used to test or demonstrate the
-#' automatic matching capabilities of the concatipede package.
+#' @section Excel file 'sequences-test-matching.xlsx':
+#'
+#' This is an Excel file (extension .xlsx) typically used to test or
+#' demonstrate the automatic matching capabilities of the concatipede package.
 #'
 #' This file represents the Excel template that could be produced by
 #' \code{\link{concatipe_prepare}} after detecting the fasta files present in a
 #' working directory.
 #'
-#' @name sequences-test-matching.xlsx
-#'
-#' @usage
-#' system.file("extdata", "sequences-test-matching.xlsx", package = "concatipede")
-#'
 #' @examples
-#' if (requireNamespace(readxl)) {
+#' # Excel file 'sequences-test-matching.xlsx'
+#' if (requireNamespace("readxl")) {
 #'   path <- system.file("extdata", "sequences-test-matching.xlsx",
 #'                       package = "concatipede")
 #'   seqs <- read_xlsx(path)
