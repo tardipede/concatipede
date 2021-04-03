d9da0817852ee3104b48b328709e0e2cc443ad22
Matteo Vecchi <matteo.vecchi15@gmail.com>
2021-04-03 19:13:53 +0300

    Now get_genbank_table also accept both a df or a filename

--- R/get_genbank_table.R
+++ R/get_genbank_table.R
@@ -2,26 +2,54 @@
 #'
 #' Extract GenBank accession number from correspondence table formatted with the same requirements for concatipede()
 #'
-#' @param corr_table a correlation table. Can be both a dataframe or a character string indicating the file path o the correspondence table in excel format
+#' @param df The user-defined correspondence table, as a data frame or equivalent. This is used only if no \code{filename} argument is provided.
+#' @param filename Filename of input correspondence table. Alternatively, if no filename is provided, the user can provide their own correspondence table as the \code{df} argument.
 #' @param writetable if TRUE save the Genbank table as excel file in the working directory
 #' @param excel.sheet specify what sheet from the excel spreadsheet you wanna read. Either a string (the name of a sheet), or an integer (the position of the sheet).
 #' @param out if writetable == T, the name to be attached to the excel filename
 #' @return Table with GenBank accession numbers
 #' @export
 
-get_genbank_table = function(corr_table, writetable = F, out = "", excel.sheet = 1){
-
-  if (is.character(corr_table)){
-    corr_table=readxl::read_xlsx(path=corr_table, col_names=TRUE, sheet = excel.sheet)
-    colnames(corr_table)=unlist(lapply(colnames(corr_table),.colname.clean))
-    corr_table=as.data.frame(apply(corr_table,2,.clean.NA))
+get_genbank_table = function(df = NULL,
+                             filename = NULL,
+                             writetable = F,
+                             out = "",
+                             excel.sheet = 1){
+  # Check that exactly one of `filename` or `df` is provided
+  if (is.null(df) & is.null(filename)) {
+    stop("Either `filename` or `df` must be provided.")
+  }
+  if (!is.null(df) & !is.null(filename)) {
+    stop("Only one of `filename` or `df` must be provided, not both.")
   }
 
-  #remove all the dataframe columns before the "name" columns
-  corr_table = corr_table[,which(colnames(corr_table)=="name"):ncol(corr_table)]
+  # If `filename`was provided
+  if (!is.null(filename)) {
+    # check if the translation table is in text format or in excel
+    if(grepl(".txt$",filename)==TRUE){
+      df=read.table(filename,header=T,sep="\t",check.names=F)
+    } else if(grepl(".xlsx$",filename)==TRUE){
+      df=readxl::read_xlsx(path=filename, sheet=excel.sheet, col_names=TRUE)
+      colnames(df)=unlist(lapply(colnames(df),.colname.clean))
+      df=as.data.frame(apply(df,2,.clean.NA))
+    } else {
+      stop("Input file format not recognized. `filename` must end with \".txt\" or \".xlsx\".")
+    }
+    } else {
+      # Check: was `df` provided by the user?
+      stopifnot(!is.null(df))
+      # Forcing df to be a data frame (things do not work properly in the rest
+      # of the function is df was given as a tibble and is not converted to a
+      # data frame)
+      df <- as.data.frame(df)}
 
-  g_table = data.frame(lapply(corr_table,.extract_accnos))
-  g_table$name = corr_table$name
+  corr_table = df
+
+  #remove all the dataframe columns before the "name" columns
+  df = df[,which(colnames(df)=="name"):ncol(df)]
+
+  g_table = data.frame(lapply(df,.extract_accnos))
+  g_table$name = df$name
 
   if(writetable==T){writexl::write_xlsx(g_table,path=paste0(out,"Genbank_accnos.xlsx"),col_names=T,format_headers=T)}
   if(writetable==F){return(g_table)}
